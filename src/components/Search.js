d45527123bbbad996a118f31352f47ce7d429997
Waqas Hassan <waqashsn@yahoo.com>
2020-12-22 14:35:22 -0500

    Installed react-redux; connected Search back into functional component and connected to Redux store

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,32 +1,54 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { getShowsAsync } from '../actions/showsActions';
 
-class Search extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
-        
-    }
+// class Search extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
 
-    handleSearchSubmit(e) {
-        e.preventDefault();
-        const keyword = e.target.elements.search_keyword.value.trim();
-        this.props.handleSearch(keyword);
-    }
+//     }
 
-    render() {
-        return (
-            <div className="input-wrapper">
-                <form onSubmit={this.handleSearchSubmit}>
-                    <input type="text" placeholder="Search..." name="search_keyword" />
-                    {/* <button>Search</button> */}
-                </form>
-            </div>
-        );
-    }
+//     handleSearchSubmit(e) {
+//         e.preventDefault();
+//         const keyword = e.target.elements.search_keyword.value.trim();
+//         this.props.handleSearch(keyword);
+//     }
 
+//     render() {
+//         return (
+//             <div className="input-wrapper">
+//                 <form onSubmit={this.handleSearchSubmit}>
+//                     <input type="text" placeholder="Search..." name="search_keyword" />
+//                     {/* <button>Search</button> */}
+//                 </form>
+//             </div>
+//         );
+//     }
+
+// }
+
+const Search = (props) => {
+    return (
+        <div className="input-wrapper">
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                props.dispatch(getShowsAsync(e.target.elements.search_keyword.value.trim()))
+            }}>
+                <input type="text" placeholder="Search..." name="search_keyword" />
+                {/* <button>Search</button> */}
+            </form>
+        </div>
+    );
 }
 
+const mapStateToProps = (state) => {
+    return {
+        keyword: state.keyword
+    }
+}
 
+export default connect(mapStateToProps)(Search);
 
 // const Search = (props) => {
 //     return (
@@ -38,6 +60,3 @@ class Search extends React.Component {
 //         </div>
 //     )
 // }
-
-
-export default Search;
--- src/index.js
+++ src/index.js
@@ -2,30 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
 // import Show from './components/Show'
 import reportWebVitals from './reportWebVitals';
 import { configureStore } from './store/configureStore';
 import { getShowsAsync, displayShow, closeShow, setError } from './actions/showsActions';
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App keyword="game thrones" />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
 const store = configureStore();
 store.subscribe(() => {
   console.log(store.getState());
 })
 
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App keyword="game thrones" />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
 // store.dispatch(getShowsAsync('x files'));
 // store.dispatch(getShowsAsync('news'));
 // store.dispatch(displayShow({ showId: "X files" }));
 // store.dispatch(setError("this is error"))
 // store.dispatch(closeShow());
-store.dispatch(getShowsAsync('bbc'));
+// store.dispatch(getShowsAsync('bbc'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
